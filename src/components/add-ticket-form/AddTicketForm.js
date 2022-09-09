15e67e2bca16e7567e20887c22dd8f63ef23c02c
dahalh <dahalh@outlook.com>
2022-09-09 12:51:57 +1000

    Add Ticket From Created

--- src/components/add-ticket-form/AddTicketForm.js
+++ src/components/add-ticket-form/AddTicketForm.js
@@ -1,54 +1,77 @@
 import React from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import "./add-ticket-form.css";
+import PropTypes from "prop-types";
 
-export const AddTicketForm = () => {
+export const AddTicketForm = ({
+  handleOnChange,
+  handleOnSubmit,
+  frmData,
+  frmDataError,
+}) => {
+  console.log(frmData);
   return (
-    <div className="jumbotron">
-      <Form
-        autoComplete="off"
-        //    onSubmit={handleOnSubmit}
-      >
-        <Form.Group className="mb-3">
-          <Form.Label>Subject</Form.Label>
-          <Form.Control
-            // onChange={handleOnChange}
-            name="subject"
-            placeholder="Enter email"
-            required
-          />
-        </Form.Group>
+    <div className="jumbotron mt-3 bg-light add-new-ticket">
+      <h1 className="text-center text-info">Add New Ticket</h1>
+      <hr />
+      <Form onSubmit={handleOnSubmit} autoComplete="off">
         <Form.Group as={Row}>
+          <Form.Label column sm={3}>
+            Subject
+          </Form.Label>
+          <Col sm={9}>
+            <Form.Control
+              name="subject"
+              value={frmData.subject}
+              // minLength="3"
+              maxLength="100"
+              onChange={handleOnChange}
+              placeholder="Subject"
+              required
+            />
+            <Form.Text className="text-danger">
+              {frmDataError.subject && "Subject is required"}
+            </Form.Text>
+          </Col>
+        </Form.Group>
+        <Form.Group className="mt-3" as={Row}>
           <Form.Label column sm={3}>
             Issue Found
           </Form.Label>
           <Col sm={9}>
             <Form.Control
-              // onChange={handleOnChange}
+              onChange={handleOnChange}
               type="date"
               name="issueDate"
+              value={frmData.issueDate}
+              required
             />
           </Col>
         </Form.Group>
-        <Form.Group as={Row}>
-          <Form.Label column sm={3}>
-            Describe
-          </Form.Label>
-          <Col sm={9}>
-            <Form.Control
-              // onChange={handleOnChange}
-              as="textarea"
-              type="date"
-              name="details"
-              rows="5"
-            />
-          </Col>
+        <Form.Group className="mt-3">
+          <Form.Label>Issue</Form.Label>
+          <Form.Control
+            onChange={handleOnChange}
+            as="textarea"
+            name="detail"
+            rows="5"
+            value={frmData.detail}
+            required
+          />
         </Form.Group>
         <div className="text-end mt-3">
-          <Button type="submit" variant="primary" block>
-            Submit
+          <Button type="submit" variant="info">
+            Open Ticket
           </Button>
         </div>
       </Form>
     </div>
   );
+};
+
+AddTicketForm.propTypes = {
+  handleOnChange: PropTypes.func.isRequired,
+  handleOnSubmit: PropTypes.func.isRequired,
+  frmData: PropTypes.object.isRequired,
+  frmDataError: PropTypes.object.isRequired,
 };
--- src/pages/new-ticket/AddTicket.js
+++ src/pages/new-ticket/AddTicket.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { AddTicketForm } from "../../components/add-ticket-form/AddTicketForm";
 import { BreadcrumbComp } from "../../components/breadcrumb/BreadcrumbComp";
+import { shortText } from "../../utils/validation";
+
+const initialFrmDt = {
+  subject: "",
+  issueDate: "",
+  detail: "",
+};
+
+const initialFrmError = {
+  subject: false,
+  issueDate: false,
+  detail: false,
+};
 
 export const AddTicket = () => {
+  const [frmData, setFrmData] = useState(initialFrmDt);
+  const [frmDataError, setFrmDataError] = useState(initialFrmError);
+
+  useEffect(() => {}, [frmData, frmDataError]);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setFrmData({ ...frmData, [name]: value });
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+
+    setFrmDataError(initialFrmError);
+
+    const isSubjectValid = await shortText(frmData.subject);
+
+    setFrmDataError({
+      ...initialFrmError,
+      subject: !isSubjectValid,
+    });
+
+    console.log("Form submit request recieved", frmData);
+  };
+
   return (
     <Container>
       <Row>
@@ -13,7 +52,12 @@ export const AddTicket = () => {
       </Row>
       <Row>
         <Col>
-          <AddTicketForm />
+          <AddTicketForm
+            handleOnChange={handleOnChange}
+            handleOnSubmit={handleOnSubmit}
+            frmDataError={frmDataError}
+            frmData={frmData}
+          />
         </Col>
       </Row>
     </Container>
